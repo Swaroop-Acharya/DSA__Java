3cfc2c04e4897a7989a6b44f204d386d2db8ffda
Swaroop Acharya <swaroopacharya935@gmail.com>
2023-06-30 14:32:35 +0530

    Basics done

---
+++ Basics/InnerClass1.java
@@ -0,0 +1,27 @@
+
+class A {
+
+    int x = 10;
+
+    class B {
+        int y = 20;
+
+        public void show() {
+            System.out.println("This is inside class");
+        }
+
+        public int access() {
+            return x;
+        }
+    }
+}
+
+public class InnerClass1 {
+    public static void main(String[] args) {
+        A outer = new A();
+        A.B inner = outer.new B();
+        System.out.println(inner.y);
+        inner.show();
+        System.out.println(inner.access());
+    }
+}
