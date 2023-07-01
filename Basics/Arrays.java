f91ff1324dffbb3ac06ce93dff24507a2248345d
Swaroop Acharya <swaroopacharya935@gmail.com>
2023-07-01 11:05:03 +0530

    Basics Done

---
+++ Basics/Arrays.java
@@ -0,0 +1,8 @@
+public class Arrays {
+    public static void main(String[] args) {
+        int[] numbers = new int[10];
+        int[] constants;
+        constants = new int[] { 1, 2, 4 };
+        int[] solo = new int[] { 1, 2, 3 };
+    }
+}
