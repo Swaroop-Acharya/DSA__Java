112a1d9010c96161050e83af05d6bb42240f2434
Swaroop Acharya <swaroopacharya935@gmail.com>
2023-07-04 12:08:25 +0530

    String method:On Progress

---
+++ String_Methods/Three.java
@@ -0,0 +1,31 @@
+package String_Methods;
+
+import java.util.StringJoiner;
+
+//String concat
+public class Three {
+    public static void main(String[] args) {
+
+        System.out.println(40 + 40 + "Swaroop" + 40 + 40);// 80Swaroop4040
+        // After a string literal, all the + will be treated as string concatenation
+        // operator.
+
+        String s1 = "A";
+        String s2 = "B";
+        System.out.println(s1.concat(s2));
+
+        String res = String.format("%s%s", s1, s2);
+        System.out.println(res);
+
+        StringBuilder s3 = new StringBuilder("Swaroop");
+        StringBuilder s4 = new StringBuilder(" Acharya");
+        System.out.println(s3.append(s4));
+
+        StringJoiner obj = new StringJoiner(","); // , is delemeter
+        obj.add("HeLLOW");
+        obj.add("Swaroop");
+        obj.add("Acharya");
+        System.out.println(obj.toString());
+
+    }
+}
